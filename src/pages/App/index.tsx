7530dcb4612af31f78456fb348585ccf05f3c46a
Matheus Correia <matheuscorreiags@gmail.com>
2022-07-02 01:32:05 -0300

    feat: initial commit with folder structures

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export const App: React.FC = () => {
+  return (
+    <Container>
+      <h1>App</h1>
+    </Container>
+  );
+};

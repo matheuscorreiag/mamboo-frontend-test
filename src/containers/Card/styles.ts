47bbe56c2a64d8edc6d79760b16a816bf2dd57fb
Matheus Correia <matheuscorreiags@gmail.com>
2022-07-04 18:01:52 -0300

    refactor: cards style changed

--- src/containers/Card/styles.ts
+++ src/containers/Card/styles.ts
@@ -6,11 +6,14 @@ interface CardProps {
 
 export const Container = styled.div<CardProps>`
   border-radius: 14px;
-  padding: 5px;
   width: 200px;
-  height: 150px;
+  height: ${(props) => (props.isEditing ? "300px" : "150px")};
   background-color: ${(props) => (props.isEditing ? "#723bff;" : "#531cb3")};
 
   transition: all;
   transition-duration: 0.3s;
+
+  color: #fff;
+
+  overflow-y: hidden;
 `;

b030767621ee85adb93520174bd041f373b687f6
Matheus Correia <matheuscorreiags@gmail.com>
2022-07-04 03:41:57 -0300

    feat: dispatch api cards

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,13 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { AnyAction } from "redux";
 import { Board } from "../../containers/Board";
 import { Header } from "../../containers/Header";
 import ThemeContext from "../../context/ThemeContext";
+import { useAppDispatch } from "../../hooks/redux";
+import { fetchTasks } from "../../store/card/actions";
 import GlobalStyle from "../../styles/themes/globalStyle";
-
 import { AppContainer } from "./styles";
 
 export const App: React.FC = () => {
   const { theme } = useContext(ThemeContext);
+
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchTasks() as unknown as AnyAction);
+  }, []);
 
   return (
     <>

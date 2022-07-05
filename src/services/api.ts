ccb33c9f2dab1021c9d87c021a60ed174487788d
Matheus Correia <matheuscorreiags@gmail.com>
2022-07-05 19:51:24 -0300

    minor: remove mock api comment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// Subi essa API para facilitar o teste, usem se preferirem no lugar de baseURL
-//https://mamboo-backend-test.herokuapp.com/
-
 const api = axios.create({
   baseURL: "http://localhost:3333/",
 });
